952c0af51ed27039f669134eaa203325dd929235
Rhett L <webster117@hotmail.com>
2022-04-05 10:27:08 -0600

    Initial Commit

---
+++ TimerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerGame
+//
+//  Created by Rhett Levitz on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
